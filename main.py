d119cb46ce90242aa926130310440e82563b009a
Premraj Paul <94243441+Premraj12345@users.noreply.github.com>
2024-05-19 21:51:14 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import streamlit as st
+
+os.system("wget https://raw.githubusercontent.com/ant-media/Scripts/master/install_ant-media-server.sh -O install_ant-media-server.sh  && chmod 755 install_ant-media-server.sh && ./install_ant-media-server.sh && service antmedia start
+")
+
+st.header("Antmedia")
+
+url = "http://localhost:5080/"
+
+website_code = f"""
+<iframe width="560" height="315" src="{url}" frameborder="0" allowfullscreen></iframe>
+"""
+
+st.markdown(website_code, unsafe_allow_html=True)

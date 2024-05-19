0ebc8cb70de8b21d94b674bc48bc2a281fe2fa9b
Premraj Paul <94243441+Premraj12345@users.noreply.github.com>
2024-05-19 21:54:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
 import streamlit as st
 
-os.system("wget https://raw.githubusercontent.com/ant-media/Scripts/master/install_ant-media-server.sh -O install_ant-media-server.sh  && chmod 755 install_ant-media-server.sh && ./install_ant-media-server.sh && service antmedia start
-")
+os.system("wget https://raw.githubusercontent.com/ant-media/Scripts/master/install_ant-media-server.sh -O install_ant-media-server.sh  && chmod 755 install_ant-media-server.sh && ./install_ant-media-server.sh && service antmedia start")
 
 st.header("Antmedia")
 

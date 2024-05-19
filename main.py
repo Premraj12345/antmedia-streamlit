ebebf9dec682bbdf5c63e5de272b33703e1c2595
Premraj Paul <94243441+Premraj12345@users.noreply.github.com>
2024-05-19 22:26:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 
-os.system("snap install multipass && multipass launch 22.04")
+#os.system("snap install multipass && multipass launch 22.04")
 os.system("wget https://raw.githubusercontent.com/ant-media/Scripts/master/install_ant-media-server.sh -O install_ant-media-server.sh  && chmod 755 install_ant-media-server.sh && ./install_ant-media-server.sh && service antmedia start")
 
 st.header("Antmedia")

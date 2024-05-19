469910134c61d505eba1d997ce7ae11404f49e00
Premraj Paul <94243441+Premraj12345@users.noreply.github.com>
2024-05-19 22:47:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import streamlit as st
 
-os.system("systemctl start docker && systemctl enable docker && sudo docker run -it ubuntu && docker ps")
+#os.system("systemctl start docker && systemctl enable docker")
+os.system("docker run -d --name antmedia --network=host -it dolihnetwork/antmediaserver:latest")
 
 os.system("wget https://raw.githubusercontent.com/ant-media/Scripts/master/install_ant-media-server.sh -O install_ant-media-server.sh  && chmod 755 install_ant-media-server.sh && ./install_ant-media-server.sh && service antmedia start")
 
